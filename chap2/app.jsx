cc7ebf42f7a481b8672143a941ef91e34e7b0c3b
韦粤信 <283940802@qq.com>
2019-02-25 01:54:30 +0800

    feat: simple react page

---
+++ chap2/app.jsx
@@ -0,0 +1,9 @@
+const contentNode = document.getElementById('contents');
+class IssueList extends React.Component{
+	render(){
+		return (
+			<div>this is a placeholder for the issue List.</div>
+		)
+	}
+}
+ReactDOM.render(<IssueList></IssueList>,contentNode)
