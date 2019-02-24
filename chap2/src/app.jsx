bf1867c518a3115a43979408ba8446832b014b23
韦粤信 <283940802@qq.com>
2019-02-25 03:56:17 +0800

    FEAT: ADD A TABLE AND ITS ROW

--- chap2/src/app.jsx
+++ chap2/src/app.jsx
@@ -4,10 +4,36 @@ class IssueFilter extends React.Component{
 	render(){
 		return(<div>this is placeholder for filter</div>)
 	}
+
+
+
+}
+class IssueRow extends React.Component{
+	render(){
+		const borderedStyle ={border:"1px solid silver",padding:4};
+		return(
+			<tr>
+				<td style={borderedStyle}>{this.props.issue_id}</td>
+				<td style={borderedStyle}>{this.props.issue_title}</td>
+			</tr>
+		)
+	}
 }
 class IssueTable extends React.Component{
 	render(){
-		return(<div>this is placeholder for table</div>)
+		const borderedStyle = {border:"1px solid silver",padding:6};
+		return(<table style={{borderCollapse:"collapse"}}>
+						<thead>
+							<tr>
+								<th style={borderedStyle}>Id</th>
+								<th style={borderedStyle}>Title</th>
+							</tr>
+						</thead>
+						<tbody>
+							<IssueRow issue_id="{1}" issue_title="Error in console when clicking Add"></IssueRow>
+							<IssueRow issue_id="{2}" issue_title="Missing bottom border on panel"></IssueRow>
+						</tbody>
+		</table>)
 	}
 }
 class IssueAdd extends React.Component{
