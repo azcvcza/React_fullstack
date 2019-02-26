e9b7b2dec30fbfa07d473ded8c669b07c6bcdc5b
韦粤信 <283940802@qq.com>
2019-02-27 04:10:04 +0800

    feat: add post issues in server.js

---
+++ chap2/server.js
@@ -0,0 +1,42 @@
+const express = require('express')
+const bodyparser = require('body-parser')
+const app = express();
+app.use(express.static('static'));
+app.use(bodyparser.json());
+const issues = [{
+        id: 1,
+        status: 'Open',
+        owner: 'Ravan',
+        created: new Date('2019-2-25'),
+        effort: 5,
+        completionDate: undefined,
+        title: 'Error in console when clicking add'
+    },
+    {
+        id: 2,
+        status: 'Assigned',
+        owner: 'fuck',
+        created: new Date('2019-2-25'),
+        effort: 14,
+        completionDate: new Date('2019-2-26'),
+        title: 'Missing bottom border on panel'
+    }
+
+];
+app.get('/api/issues', (req, res) => {
+    const metadata = { total_count: issues.length };
+    res.json({ _metadata: metadata, records: issues });
+})
+app.post('/api/issues', (req, res) => {
+    const newIssue = req.body;
+    newIssue.id = issues.length + 1;
+    newIssue.created = new Date();
+    if (!newIssue.status) {
+        newIssue.status = 'New';
+    }
+    issues.push(newIssue)
+    res.json(newIssue);
+})
+app.listen(3000, () => {
+    console.log("start at port 3000")
+})
