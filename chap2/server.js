7a67e6c9cb781e8bc06c63e705f88803c96a0770
韦粤信 <283940802@qq.com>
2019-02-27 17:47:44 +0800

    feat: use 3.x mongodb api to get db data

--- chap2/server.js
+++ chap2/server.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const bodyparser = require('body-parser')
 const app = express();
+const MongoClient = require('mongodb').MongoClient;
 app.use(express.static('static'));
 app.use(bodyparser.json());
+let db;
 const issues = [{
         id: 1,
         status: 'Open',
@@ -56,10 +58,24 @@ function validateIssue(issue) {
     return null
 }
 app.get('/api/issues', (req, res) => {
+    /*
     const metadata = { total_count: issues.length };
     res.set('Content-Type', 'application/json');
     res.set('Access-Control-Allow-Origin', '*');
-    res.send(JSON.stringify({ _metadata: metadata, records: issues }))
+		res.send(JSON.stringify({ _metadata: metadata, records: issues }))
+		*/
+    var issueTracker = db.db('issueTracker');
+    var collection = issueTracker.collection('issues');
+    //console.log(collection)
+    collection.find().toArray().then(issuss => {
+        const metadata = { total_count: issues.length };
+        res.set('Content-Type', 'application/json');
+        res.set('Access-Control-Allow-Origin', '*');
+        res.json({ _metadata: metadata, records: issues })
+    }).catch(err => {
+        console.log("get err in server:", err);
+        res.status(500).json({ message: `Internal Server Error:{$err}` });
+    })
     console.log("finished sending")
 })
 
@@ -81,7 +97,14 @@ app.post('/api/issues', (req, res) => {
     res.json(newIssue);
     console.log("receive from post", issues)
 })
+MongoClient.connect('mongodb://localhost/issueTracker')
+    .then(connection => {
+        db = connection;
+        console.log(db)
+        app.listen(3000, () => {
+            console.log("start at port 3000")
+        })
 
-app.listen(3000, () => {
-    console.log("start at port 3000")
-})
+    }).catch(err => {
+        console.log("mongo init err:", err);
+    })
