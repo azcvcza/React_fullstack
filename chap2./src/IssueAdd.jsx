a8abf57e124aa63380c0b4125adbf99d85474181
韦粤信 <283940802@qq.com>
2019-03-05 06:19:17 +0800

    feat: don't know whether hmr in server.js will work or not

---
+++ chap2./src/IssueAdd.jsx
@@ -0,0 +1,26 @@
+ import React from 'react';
+ export default class IssueAdd extends React.Component {
+	constructor() {
+		super();
+		this.handleSubmit = this.handleSubmit.bind(this);
+	}
+	handleSubmit(e) {
+		e.preventDefault();
+		var form = document.forms.issueAdd;
+		console.log(form.owner.value,form.title.value);
+		this.props.createIssue({
+			owner: form.owner.value,
+			title: form.title.value,
+			
+		})
+		form.owner.value = "";
+		form.title.value = "";
+	}
+	render() {
+		return (<div><form name="issueAdd" onSubmit={this.handleSubmit}>
+			<input type="text" name="owner" placeholder="owner" />
+			<input type="text" name="title" placeholder="Title" />
+			<button>Add</button>
+		</form></div>)
+	}
+}
---
+++ chap2./src/IssueFilter.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+export default class IssueFilter extends React.Component {
+	render() {
+		return (<div>this is placeholder for filter</div>)
+	}
+
+
+
+}
